6cf37c773168afa9016110ea8257ef5310c52091
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-11-10 22:31:30 +1100

    Contact Filer Form & State

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -111,7 +111,7 @@ const ContactState = props => {
       value={{
         contacts: state.contacts,
         current: state.current,
-        filtered: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
