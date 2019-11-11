003adec2b5e1e36c7eee20cc9d5d620946e23314
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-11-11 23:03:55 +1100

    Auth Context & Initial State

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ContactState from "./context/contact/ContactState";
 import "./App.css";
 
 import Navbar from "./component/layout/Navbar";
 import Home from "./component/pages/Home";
 import About from "./component/pages/About";
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
