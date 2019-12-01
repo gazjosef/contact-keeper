64f15d392bc134a9b8e334b30ded7a73000287c4
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-12-01 21:49:55 +1100

    Load User & Set Token

--- client/src/context/auth/AuthReducer.js
+++ client/src/context/auth/AuthReducer.js
@@ -3,9 +3,9 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT,
+  // LOGIN_SUCCESS,
+  // LOGIN_FAIL,
+  // LOGOUT,
   CLEAR_ERRORS
 } from "../types";
 
--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.get("/", auth, async (req, res) => {
 router.post(
   "/",
   [
-    check("email", "PLease include a valid email").isEmail(),
+    check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists()
   ],
   async (req, res) => {
@@ -41,13 +41,13 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
-        return res.status(400).json({ msg: "invalid credentials" });
+        return res.status(400).json({ msg: "Invalid Credentials" });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ msg: "Invalid credentials" });
+        return res.status(400).json({ msg: "Invalid Credentials" });
       }
 
       const payload = {
