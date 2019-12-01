d016ee6b812b0235f84e9d58291db19d29efa503
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-12-01 22:23:58 +1100

    User Login

--- client/src/context/auth/AuthReducer.js
+++ client/src/context/auth/AuthReducer.js
@@ -3,8 +3,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
-  // LOGIN_SUCCESS,
-  // LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
   // LOGOUT,
   CLEAR_ERRORS
 } from "../types";
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -9,8 +9,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
-  // LOGIN_SUCCESS,
-  // LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
   // LOGOUT,
   CLEAR_ERRORS
 } from "../types";
